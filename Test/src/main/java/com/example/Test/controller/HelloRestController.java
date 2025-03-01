945d62adf69339c95716836cfe79266d0a2fc31b
Shailendrachaudhary11 <shailendrachaurdhary0008@gmail.com>
2025-03-01 22:27:56 +0530

    Assignmet of uC2

--- Test/src/main/java/com/example/Test/controller/HelloRestController.java
+++ Test/src/main/java/com/example/Test/controller/HelloRestController.java
@@ -16,4 +16,9 @@ public class HelloRestController {
         return "Hello From BridgeLabz! ! !";
 
     }
+
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name){
+        return "Hello " + name+" from Bridgelabcz";
+    }
 }
